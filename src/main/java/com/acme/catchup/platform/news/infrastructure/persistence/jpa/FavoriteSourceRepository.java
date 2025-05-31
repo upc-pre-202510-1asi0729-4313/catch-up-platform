11b4b90f4c7a74836444563458d046ea577365f5
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-05-31 14:59:39 -0500

    feat: add repository

---
+++ src/main/java/com/acme/catchup/platform/news/infrastructure/persistence/jpa/FavoriteSourceRepository.java
@@ -0,0 +1,9 @@
+package com.acme.catchup.platform.news.infrastructure.persistence.jpa;
+
+import com.acme.catchup.platform.news.domain.model.aggregate.FavoriteSource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteSourceRepository extends JpaRepository<FavoriteSource, Long> {
+}
