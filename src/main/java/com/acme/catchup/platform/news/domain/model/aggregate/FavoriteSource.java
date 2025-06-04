a9fbbf60bc5410608b2295610f5068e770d65f27
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-04 18:56:39 -0500

    fix: auditing for entity

--- src/main/java/com/acme/catchup/platform/news/domain/model/aggregate/FavoriteSource.java
+++ src/main/java/com/acme/catchup/platform/news/domain/model/aggregate/FavoriteSource.java
@@ -5,11 +5,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class FavoriteSource {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
